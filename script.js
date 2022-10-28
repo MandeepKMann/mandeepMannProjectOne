d3099263f4f5990761148f11df551d55a4268da8
MandeepMann <mandeep_mann30@hotmail.com>
2022-10-28 19:49:29 -0400

    NOTHING is working, trying a different tutorial

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-const galleryImg = document.querySelectorAll('.gallery img');
-const modal = document.querySelector('.modal');
-const popUpImg = document.querySelector('.popUpImg').src;
-console.log(popUpImg)
+// const galleryImg = document.querySelectorAll('.gallery img');
+// const modal = document.querySelector('.modal');
+// const popUpImg = document.querySelector('.popUpImg');
+// console.log(popUpImg)
 
 
-galleryImg.forEach(image =>{
-    image.onclick = () =>{
-        modal.style.display = 'block';
-        popUpImg.src = image.getAttribute('src');
-    }
-});
+// galleryImg.forEach(image =>{
+//     image.onclick = () =>{
+//         modal.style.display = 'block';
+//         popUpImg.src = image.getAttribute('src');
+//     }
+// });
 
-const closeModal = document.querySelectorAll('.modal i');
+// const closeModal = document.querySelectorAll('.modal i');
 
-closeModal.forEach(button =>{
-    button.onclick = () =>{
-        modal.style.display = 'none';
-    }
-})
+// closeModal.forEach(button =>{
+//     button.onclick = () =>{
+//         modal.style.display = 'none';
+//     }
+// })
 
-console.log(closeModal);
+
+const lightbox = document.createElement('div');
+
+const gallery = document.querySelector('.gallerySection');
+
+lightbox.id = 'lightbox';
+
+gallery.append(lightbox)
 
 
 
