48deb50b2a889dde1de7c853b8737c9093f9e348
MandeepMann <mandeep_mann30@hotmail.com>
2022-10-28 21:48:45 -0400

    toTop button WORKS but won't go away when at top :triumph:

--- script.js
+++ script.js
@@ -1,59 +1,70 @@
-// const galleryImg = document.querySelectorAll('.gallery img');
-// const modal = document.querySelector('.modal');
-// const popUpImg = document.querySelector('.popUpImg');
-// console.log(popUpImg)
+// Lightbox JS
 
-
-// galleryImg.forEach(image =>{
-//     image.onclick = () =>{
-//         modal.style.display = 'block';
-//         popUpImg.src = image.getAttribute('src');
-//     }
-// });
-
-// const closeModal = document.querySelectorAll('.modal i');
-
-// closeModal.forEach(button =>{
-//     button.onclick = () =>{
-//         modal.style.display = 'none';
-//     }
-// })
+// This video saved me https://www.youtube.com/watch?v=uKVVSwXdLr0
 
 // create div and variable and give it an id
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
 
 // append it to the bodyyyyy
-document.body.appendChild(lightbox)
+document.body.appendChild(lightbox);
 
 // select gallery imgs
-const galleryImages = document.querySelectorAll('.gallery img')
+const galleryImages = document.querySelectorAll('.gallery img');
 
 // put on event listener on each gallery img
 galleryImages.forEach(galleryImages => {
     galleryImages.addEventListener('click', e => {
         // when clicked, add active class which shows lightbox
-        lightbox.classList.add('active')
+        lightbox.classList.add('active');
         // create img element
-        const img = document.createElement('img')
+        const img = document.createElement('img');
         // make the src for newly created image same as the one clicked
-        img.src = galleryImages.src
+        img.src = galleryImages.src;
         // makes it so while one image is clicked, you can't click another and have them both show up (removes img before opening another img)
         while (lightbox.firstChild) {
-            lightbox.removeChild(lightbox.firstChild)
+            lightbox.removeChild(lightbox.firstChild);
         }
         // append newly created img to lightbox as child
-        lightbox.appendChild(img)
-    })
-})
+        lightbox.appendChild(img);
+    });
+});
 
 // put event listener on lightbox
 lightbox.addEventListener('click', e => {
     // if click on image, do nothing, if click on lightbox, close
     if (e.target !== e.currentTarget) return
-    lightbox.classList.remove('active')
-})
+    lightbox.classList.remove('active');
+});
 
+
+// Scroll to the TOP!
+
+// Create scroll button and give id and append to body
+const scrollButton = document.createElement('button');
+scrollButton.id = 'toTop'
+document.body.appendChild(scrollButton);
+
+// create icon and give font awesome classes and append as child to scroll button
+const scrollButtonIcon = document.createElement('i');
+scrollButtonIcon.className = ('fa-solid fa-chevron-up');
+scrollButton.appendChild(scrollButtonIcon);
+
+// scroll down to a certain point before the scroll to top button appears
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        scrollButton.classList.add('active');
+    };
+});
+
+// have scroll button listen for click then scroll to top, smooooooooothly
+scrollButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
 
 
 
