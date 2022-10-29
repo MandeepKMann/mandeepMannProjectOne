efdac38dc83e6c3caf852a38f18a0410d31dfe01
MandeepMann <mandeep_mann30@hotmail.com>
2022-10-28 20:52:55 -0400

    everything worked then I broke it and realised I had unlinked my js....oops

--- script.js
+++ script.js
@@ -19,14 +19,41 @@
 //     }
 // })
 
+// create div and variable and give it an id
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
 
-const lightbox = document.createElement('div');
+// append it to the bodyyyyy
+document.body.appendChild(lightbox)
 
-const gallery = document.querySelector('.gallerySection');
+// select gallery imgs
+const galleryImages = document.querySelectorAll('.gallery img')
 
-lightbox.id = 'lightbox';
+// put on event listener on each gallery img
+galleryImages.forEach(galleryImages => {
+    galleryImages.addEventListener('click', e => {
+        // when clicked, add active class which shows lightbox
+        lightbox.classList.add('active')
+        // create img element
+        const img = document.createElement('img')
+        // make the src for newly created image same as the one clicked
+        img.src = galleryImages.src
+        // makes it so while one image is clicked, you can't click another and have them both show up (removes img before opening another img)
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        // append newly created img to lightbox as child
+        lightbox.appendChild(img)
+    })
+})
 
-gallery.append(lightbox)
+// put event listener on lightbox
+lightbox.addEventListener('click', e => {
+    // if click on image, do nothing, if click on lightbox, close
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
+
 
 
 
